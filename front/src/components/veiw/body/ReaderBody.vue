86ac58483b548a59333d8cd8603fd5a0da581b59
ppcat <wuyifan96246@163.com>
2023-05-20 16:35:05 +0800

    添加了阅读器页面

---
+++ front/src/components/veiw/body/ReaderBody.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="fixparent">
+    <ElAffix>
+      <ReaderMenu />
+    </ElAffix>
+    <!-- 包裹在容器中，限定宽高，并传入文本内容 -->
+    <ReaderComponent />
+
+  </div>
+</template>
+
+<script>
+import ReaderComponent from './reader/ReaderComponent.vue';
+import ReaderMenu from './reader/ReaderMenu.vue';
+import { ElAffix } from 'element-plus';
+export default {
+  name: "ReaderBody",
+  data() {
+    return {
+      content: {
+        chapters: [],
+        text: "",
+      },
+    };
+  },
+  components: {
+    ReaderComponent,
+    ReaderMenu,
+    ElAffix,
+  },
+}
+</script>
