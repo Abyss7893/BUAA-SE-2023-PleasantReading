d452007586beb1d7aa7008b6f0e67ec2ac547216
ppcat <wuyifan96246@163.com>
2023-05-21 18:49:05 +0800

    0

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -8,6 +8,40 @@ const store = createStore({
       userAvatar: null,
       navigationLoc: 1,
       categoriesIndex: [0, 0, 0, 0],
+      themes: [
+        {
+          "name": "默认",
+          "color": "hsla(40,60%,95%,.8)",
+          "bg1": require('assets/imgs/readerbcg/default/body.png'),
+          "bg2": require('assets/imgs/readerbcg/default/basic.png'),
+        },
+        {
+          "name": "牛皮纸",
+          "color": "hsla(44,67%,88%,.8)",
+          "bg1": require('assets/imgs/readerbcg/lightyellow/body.png'),
+          "bg2": require('assets/imgs/readerbcg/lightyellow/basic.png'),
+        },
+        {
+          "name": "淡绿色",
+          "color": "rgba(230,242,230,.8)",
+          "bg1": require('assets/imgs/readerbcg/lightgreen/body.png'),
+          "bg2": require('assets/imgs/readerbcg/lightgreen/basic.png'),
+        },
+        {
+          "name": "淡蓝色",
+          "color": "rgba(228,241,245,.8)",
+          "bg1": require('assets/imgs/readerbcg/lightblue/body.png'),
+          "bg2": require('assets/imgs/readerbcg/lightblue/basic.png'),
+        },
+        {
+          "name": "淡粉色",
+          "color": "hsla(0,46%,93%,.8)",
+          "bg1": require('assets/imgs/readerbcg/lightpink/body.png'),
+          "bg2": require('assets/imgs/readerbcg/lightpink/basic.png'),
+        },
+      ],
+      readerSettings: [null, "default", 18, 800],
+
     }
   },
   mutations: {
@@ -34,7 +68,11 @@ const store = createStore({
     },
     changeCategoriesIndex(state, content) {
       state.categoriesIndex[content.title] = content.index
+    },
+    setReaderSettings(state, settings) {
+      state.readerSettings = settings
     }
+
   }
 })
 export default store
