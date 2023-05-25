058daaf36a729038dcf086b904b975a467fff59d
leeibo <leeibo@qq.com>
2023-05-25 14:02:53 +0800

    完善了我的收藏的路由

--- front/src/components/veiw/body/mybooks/MyBookBody.vue
+++ front/src/components/veiw/body/mybooks/MyBookBody.vue
@@ -7,7 +7,7 @@
         <el-empty description="暂无书籍" />
       </div>
       <div v-else>
-        <el-row v-for="ro in rownum - 1" :key="ro" gutter="60">
+        <el-row v-for="ro in rownum -1" :key="ro" gutter="60">
           <el-col
             v-for="o in colsize"
             :key="(ro - 1) * colsize + o - 1"
@@ -16,21 +16,23 @@
             <book-card
               :cover="mybooks[(ro - 1) * colsize + o - 1].cover"
               :name="mybooks[(ro - 1) * colsize + o - 1].name"
+              :bookid="mybooks[(rownum - 1) * colsize + o - 1].bookid"
             ></book-card>
           </el-col>
           <el-divider />
         </el-row>
-        <div v-if="mybooks.length % colsize > 0">
+        <div v-if="(mybooks.length-(rownum-1)*colsize) > 0">
           <el-row gutter="60">
             <el-col
-              v-for="o in mybooks.length % colsize"
+              v-for="o in (mybooks.length-(rownum-1)*colsize)"
               :key="(rownum - 1) * colsize + o - 1"
               :span="24 / colsize"
             >
               <book-card
                 :cover="mybooks[(rownum - 1) * colsize + o - 1].cover"
                 :name="mybooks[(rownum - 1) * colsize + o - 1].name"
-                @click="gotoRead"
+                :bookid="mybooks[(rownum - 1) * colsize + o - 1].bookid"
+              
               />
             </el-col>
             <el-divider />
@@ -58,11 +60,7 @@ export default {
     };
   },
   methods: {
-    gotoRead() {
-      this.$router.push({
-        path: "/reader/" + this.bookId + "/1",
-      });
-    },
+
   },
   created() {
     // for (let i = 0; i < 99; i++) {
@@ -79,6 +77,7 @@ export default {
           this.mybooks.push(book);
         }
         this.rownum = Math.ceil(this.mybooks.length / this.colsize);
+        console.log(this.rownum);
       });
     });
   },
