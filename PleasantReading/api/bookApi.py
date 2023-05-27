38f7938148728129581d3b1ec57355bdb99ec261
韩一 <20376290>
2023-05-27 16:59:21 +0800

    fix author

--- PleasantReading/api/bookApi.py
+++ PleasantReading/api/bookApi.py
@@ -407,4 +407,4 @@ def getAuthor(request, name):
     works = BookBasicInfo.objects.filter(author=name, onShelf=True).values('bookID', 'name', 'profile', 'img')
     if res.count() == 0:
         return JsonResponse({'profile': '暂无有关详细信息', 'img': 'AuthorImg/default.jpg', 'works': list(works)})
-    return {'profile': res.first().profile, 'img': res.first().img, 'works': list(works)}
+    return JsonResponse({'profile': res.first().profile, 'img': res.first().img, 'works': list(works)})
